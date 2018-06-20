28f7f9d496d27f804b9504cb472b51754f6224de
CastleMY <490523604@qq.com>
2018-06-20 23:01:41 +0800

    微信服务库

--- src/index.ts
+++ src/index.ts
@@ -104,7 +104,12 @@ if (IsWechatBrower) {
         jsConfig()
     })
 }
-export function location(s: Function, e: Function) {
+/**
+ * https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421141115
+ * @param s 
+ * @param e 
+ */
+export function location(s: (res: { latitude: number, longitude: number, speed: number, accuracy: number }) => void, e?: Function) {
     if (!IsWechatBrower) {
         throw new Error('NOT_WECHAT_BROWER');
     }
@@ -117,7 +122,7 @@ export function location(s: Function, e: Function) {
         }
     })
 }
-export function scan(s: Function, NeedResult: boolean = false, e?: Function) {
+export function scan(s: (result: string) => void, NeedResult: boolean = false, e?: Function) {
     if (!IsWechatBrower) {
         throw new Error('NOT_WECHAT_BROWER');
     }
@@ -170,4 +175,44 @@ function uuid() {
 
     var uuid = s.join("");
     return uuid;
+}
+/**
+ * 选择图片，弹出微信图片选择框
+ * @param success 
+ * @param count 
+ */
+export function chooseImage(success: (src: string[]) => void, count: number = 9) {
+    wx.chooseImage({
+        count: count || 9, // 默认9
+        sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+        sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+        success: (res: any) => {
+            success(res.localIds)
+        }
+    })
+}
+/**
+ * 上传图片
+ * @param localIds  需要上传的图片的本地ID，由chooseImage接口获得
+ * @param success 
+ */
+export function uploadImage(localIds: string[], success: (src: string[]) => void) {
+    wx.uploadImage({
+        localId: localIds, // 需要上传的图片的本地ID，由chooseImage接口获得
+        isShowProgressTips: 1, // 默认为1，显示进度提示
+        success: (res: any) => {
+            success(res.serverId)
+        }
+    });
+}
+/**
+ * 预览图片
+ * @param current 当前显示图片的http链接
+ * @param urls 需要预览的图片http链接列表
+ */
+export function previewImage(current: string, urls: string[]) {
+    wx.previewImage({
+        current: current,
+        urls: urls
+    })
 }
